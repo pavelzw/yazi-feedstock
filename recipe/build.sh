dc3b04788e83af7bfc14531eab1f79178efe330c
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-08-16 08:50:52 +0200

    set VERGEN_GIT_SHA environment variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@
 set -euxo pipefail
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
+export VERGEN_GIT_SHA="v${PKG_VERSION}"
 
 cargo install --path "${SRC_DIR}/yazi-build" --no-track --locked --bins yazi-build
 cargo install --path "${SRC_DIR}/yazi-fm" --no-track --locked --bins --root "${PREFIX}" yazi-fm
